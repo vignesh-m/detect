be59b5be4e7e698d49cc8b43e0367442468b2449
vignesh-m <vicky.mano@gmail.com>
2018-01-07 15:32:48 +0530

    chenge to video

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from flask import Flask, request
 from flask import render_template
 import os
+from subprocess import call
 
 app = Flask(__name__)
 
@@ -10,12 +11,16 @@ def main():
 
 @app.route('/submit', methods=['POST'])
 def submit():
-    print(request.form)
-    print(request.files)
-    name = request.form['name']
-    image = request.files['image']
-
-    image.save('storage/{}.jpg'.format(name))
+    video = request.files['video']
+    filename = 'storage/{}'.format(video.filename)
+    video.save(filename)
+    img_dir = os.path.splitext(filename)[0]
+    print(img_dir)
+    try:
+        os.mkdir(img_dir)
+    except OSError:
+        pass
+    call(['ffmpeg', '-i', filename, '-vf', 'fps=3', img_dir+'/out%d.jpg'])
     return 'hello'
 
 def main():
